1ea1502519cd92132aa0723043a4114b25426e11
shatrujeet <shatru21217@gmail.com>
2023-09-09 13:04:37 +0530

    day4 completed

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -8,39 +8,53 @@ class LoginPage extends StatelessWidget {
     return Material(
       child: Column(
         children: [
-          Image.asset('assets/images/login_image.png',fit:BoxFit.cover ,),
+          Image.asset(
+            'assets/images/login_image.png',
+            fit: BoxFit.cover,
+          ),
           SizedBox(
             height: 20,
           ),
-          Text('Welcome',style:TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
-          )),
-           SizedBox(
+          Text('Welcome',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              )),
+          SizedBox(
             height: 20,
           ),
-         Padding(
-           padding: const EdgeInsets.symmetric(horizontal: 32,vertical:16),
-           child: Column(
-            children: [
-               TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Enter username',
-                labelText: 'Username'
-              )
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+            child: Column(
+              children: [
+                TextFormField(
+                    decoration: InputDecoration(
+                        hintText: 'Enter username', labelText: 'Username')),
+                TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Enter Password',
+                      labelText: 'Password',
+                    )),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    print('hey');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                  ),
+                  child: Text(
+                    'login',
+                    style: TextStyle(fontWeight: FontWeight.normal),
+                  ),
+                ),
+              ],
             ),
-              TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Enter Password',
-                labelText: 'Password',
-              )
-            ),  
-            ],
-           ),
-         )
+          )
         ],
-        
-        
       ),
     );
   }
